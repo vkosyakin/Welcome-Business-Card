43b76d88280cb8d5a7e9f85bade61feb6ad92baf
Valeriy Kosyakin <valeradzu@mail.ru>
2021-05-29 14:53:50 +0300

    Initial Commit

---
+++ Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Валера on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
